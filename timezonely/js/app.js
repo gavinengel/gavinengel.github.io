f884d041ddfd36a06bbffa89922ebfc6116fbe00
Gavin Engel <gavin@engel.com>
2015-05-17 06:42:35 -0400

    .

--- timezonely/js/app.js
+++ timezonely/js/app.js
@@ -146,7 +146,19 @@ angular.module('timezonely', ['ngRoute', 'firebase', 'ui.bootstrap'])
     };
 
     $scope.getLocalTime = function(timezone) {
-        return 'timezone id is:'+timezone.$id;
+
+        $scope.clock = "loading clock..."; // initialise the time variable
+        $scope.tickInterval = 1000 //ms
+
+        var tick = function () {
+            $scope.clock = Date.now() // get the current time
+            $timeout(tick, $scope.tickInterval); // reset the timer
+        }
+
+        // Start the timer
+        $timeout(tick, $scope.tickInterval);
+
+        return $timeout; //'timezone id is:'+timezone.$id;
 
     };
 

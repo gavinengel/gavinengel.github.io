62725737fd1877f16f4beb93784bce7c81cb304b
Gavin Engel <gavin@engel.com>
2015-05-27 18:08:02 -0400

    .

--- timezonely/js/app.js
+++ timezonely/js/app.js
@@ -353,51 +353,23 @@ var array = JSON.stringify([ 'foo', 'bar' ]);
 
 //var encoded = encodeURIComponent(JSON.stringify(timezone2))
          $http({
-                url: getUrl()+'?username=gavin&password=engel',
-                method: "POST",
-                //data: "&city=" + encodeURIComponent(timezone.city) + "&designation=" + encodeURIComponent(timezone.designation) +"&difference=" + encodeURIComponent(timezone.difference) +"&zonename=" + encodeURIComponent(timezone.zonename) , //timezone2,//encoded,
-                //headers: {'Content-Type':  'application/x-www-form-urlencoded'} //  'application/json'}
-                //data: postObject,
-                params: { data: array },
+            url: getUrl()+'?username=gavin&password=engel',
+            method: "POST",
+            //data: "&city=" + encodeURIComponent(timezone.city) + "&designation=" + encodeURIComponent(timezone.designation) +"&difference=" + encodeURIComponent(timezone.difference) +"&zonename=" + encodeURIComponent(timezone.zonename) , //timezone2,//encoded,
+            //headers: {'Content-Type':  'application/x-www-form-urlencoded'} //  'application/json'}
+            //data: postObject,
+            params: { data: array },
 
-                //headers: {'Content-Type':  'application/json'}
-                }).success(function (data, status, headers, config) {
-                    console.log(data);
+            //headers: {'Content-Type':  'application/json'}
+            }).success(function (data, status, headers, config) {
+                console.log(data);
 
-                }).error(function (data, status, headers, config) {});
-
-
-
-         $http({
-                url: getUrl()+'?username=gavin&password=engel',
-                method: "POST",
-                //data: "&city=" + encodeURIComponent(timezone.city) + "&designation=" + encodeURIComponent(timezone.designation) +"&difference=" + encodeURIComponent(timezone.difference) +"&zonename=" + encodeURIComponent(timezone.zonename) , //timezone2,//encoded,
-                //headers: {'Content-Type':  'application/x-www-form-urlencoded'} //  'application/json'}
-                //data: postObject,
-                data: { data: array },
-
-                //headers: {'Content-Type':  'application/json'}
-                }).success(function (data, status, headers, config) {
-                    console.log(data);
-
-                }).error(function (data, status, headers, config) {});
+            }).error(function (data, status, headers, config) {});
 
 
-                         $http({
-                url: getUrl()+'?username=gavin&password=engel',
-                method: "POST",
-                //data: "&city=" + encodeURIComponent(timezone.city) + "&designation=" + encodeURIComponent(timezone.designation) +"&difference=" + encodeURIComponent(timezone.difference) +"&zonename=" + encodeURIComponent(timezone.zonename) , //timezone2,//encoded,
-                headers: {'Content-Type':  'application/x-www-form-urlencoded'} //  'application/json'}
-                data: postObject,
-                //data: { data: array },
-
-                //headers: {'Content-Type':  'application/json'}
-                }).success(function (data, status, headers, config) {
-                    console.log(data);
-
-                }).error(function (data, status, headers, config) {});
 
 
+                
 /*
             return $http.post(getUrl()+'?username=gavin&password=engel', {city:'grrrr'});
             return $resource(getUrl()+'?username=gavin&password=engel', {}, {

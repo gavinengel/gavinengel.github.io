228ad3d2900c6204317b9d3accb87b511c47da5d
Gavin Engel <gavin@engel.com>
2015-05-27 21:00:24 -0400

    .

--- timezonely/js/factories/factories.js
+++ timezonely/js/factories/factories.js
@@ -3,5 +3,5 @@ timezonelyApp.factory('Users', function($firebase, fbURL, user_table) {
 })
 
 timezonelyApp.factory('Timezones', function($firebase, fbURL, timezone_table) {
-  return $firebase(new Firebase(fbURL + timezone_table));
+  //return $firebase(new Firebase(fbURL + timezone_table));
 })

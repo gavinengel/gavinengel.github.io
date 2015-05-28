be306ffe088e80146941cd22eab0ae95aea29032
Gavin Engel <gavin@engel.com>
2015-05-27 22:47:32 -0400

    .

--- timezonely/js/controllers/controllers.js
+++ timezonely/js/controllers/controllers.js
@@ -4,6 +4,7 @@ timezonelyApp.controller('TimezoneCtrl', function($scope, $modal, $location, Tim
     // Define valriables
     $scope.alerts = [];     // array of alert message objects.
     $scope.timezones = Timezones;
+    $scope.dashboard = {}
 
     //$scope.dashboard.test = {{city: 'asdf'},{city: 'asdffff'},{city: 'ffffffffffff'}} // debug
 

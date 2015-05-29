6447a9d82ccf8c1cb94d96786f8764f75bb8a155
Gavin Engel <gavin@engel.com>
2015-05-28 22:36:02 -0400

    .

---
+++ timezonely/README.md
@@ -0,0 +1,4 @@
+# Timezonely app
+To view this project go here: http://code.gavinengel.com/timezonely/
+
+The API is located here: http://express.api.timezonely.engeldev.com/api/
--- timezonely/js/app.js
+++ timezonely/js/app.js
@@ -3,7 +3,7 @@ var timezonelyApp = angular.module('timezonely', ['ngRoute', 'firebase', 'ui.boo
 .value('user_table', 'user')
 .value('timezone_table', 'timezone')
 .value('envoy', {name:'envoy'}) // carries data to views
-.constant('ENDPOINT_URI', 'http://localhost:9000/api/')//http://express.api.timezonely.engeldev.com/api/') // this is the timezones api http://ovh.engeldev.com:9000/api/
+.constant('ENDPOINT_URI', 'http://express.api.timezonely.engeldev.com/api/') // this is the timezones api http://ovh.engeldev.com:9000/api/
 .config(function($routeProvider) {
     $routeProvider
     .when('/home', {

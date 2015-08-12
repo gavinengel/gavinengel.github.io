70e4b7f28a678c0dd6db9108bb1b82aab2cb436a
Gavin Engel <gavin@engel.com>
2015-08-11 21:55:42 -0400

    .

--- cv/js/custom.js
+++ cv/js/custom.js
@@ -105,11 +105,11 @@ $(document).ready(function() {
       }
     })
 
-    // Insurance
-    $('#insurance .pricing-table h3').html(resume.sections.insurance.proof) // proof: "http://bit.ly/gavinengelinsurance"
-    $('#insurance .pricing-table ul').append('<li>'+resume.sections.insurance.provider+'</li>') // provider: "Hiscox Insurance Company Inc."
-    $('#insurance .pricing-table ul').append('<li>'+resume.sections.insurance.type+'</li>') //       type: "Aggregate limit of liability:  $250,000"
-    $('#insurance .pricing-table ul').append('<li>'+resume.sections.insurance.refid+'</li>') // refid: "UDC­1616273­EO­15"
+    // Insurance 
+    $('#insurance .pricing-table h3').html(resume.sections.insurance.provider)                // provider: "Hiscox Insurance Company Inc."
+    $('#insurance .pricing-table ul').append('<li>'+resume.sections.insurance.type+'</li>')   // type: "Aggregate limit of liability:  $250,000"
+    $('#insurance .pricing-table ul').append('<li>'+resume.sections.insurance.refid+'</li>')  // refid: "UDC­1616273­EO­15"
+    $('#insurance .pricing-table ul').append('<li>'+resume.sections.insurance.proof+'</li>')  // proof: "http://bit.ly/gavinengelinsurance"
 
 
     // Professional Experience
